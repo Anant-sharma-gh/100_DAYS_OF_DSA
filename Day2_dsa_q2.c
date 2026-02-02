7620c795b4f687fe6f8b931a4e5d5952477b7ba4
Anant-sharma-gh <Anant.23300@stu.upes.ac.in>
2026-02-02 22:52:32 +0530

    Add files via upload

---
+++ Day2_dsa_q2.c
@@ -0,0 +1,12 @@
+int maxProfit(int* prices, int pricesSize) {
+ int  profit = 0,minprice = prices[0];
+ for (int i =1;i<pricesSize;i++){
+    if(prices[i]<minprice){
+        minprice = prices[i];
+    }
+    if(prices[i] - minprice> profit){
+        profit=prices[i]-minprice;
+    }
+ }
+ return profit;
+}
