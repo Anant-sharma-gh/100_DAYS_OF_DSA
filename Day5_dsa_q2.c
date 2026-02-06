47ba086437e0e81386eb184f3583583fd1f25e8b
Anant-sharma-gh <anantworks2207@outlook.com>
2026-02-07 00:28:53 +0530

    Day 5 questions

---
+++ Day5_dsa_q2.c
@@ -0,0 +1,22 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+  int l=m+n-1;
+
+  while(m>0 && n>0){
+    if(nums1[m-1]>nums2[n-1]){
+        nums1[l]=nums1[m-1];
+        m--;
+    }
+    else{
+        nums1[l]=nums2[n-1];
+        n--;
+    }
+    l--;
+  }
+
+  while(n>0){
+    nums1[l]=nums2[n-1];
+    n--;
+    l--;
+  }
+
+}
